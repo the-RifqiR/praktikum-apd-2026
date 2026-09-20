1ff312ed041848abfa7a90a40a86ee2ae68a1989
the-RifqiR <rifqi00f@gmail.com>
2026-09-20 11:28:09 +0800

    commit pt-2 first

--- pertemuan1.py
+++ pertemuan1.py
@@ -1,1 +1,97 @@
-print("Butuh her")
+# #print("Butuh her")
+
+# #Numerik
+# #int bilangan bulat
+
+# angka1 = 5
+# angka2 = 10
+# hasil = angka1 + angka2
+# print(hasil)
+
+
+# #float
+# #mirip int tapi pecahan
+# a = 3.45
+# b = 1.23
+# hitung = a + b
+# print(hitung)
+
+# #string
+# #untuk karakter huruf
+# nama = "Larry"
+# hobi = "ngoding"
+# print(nama)
+# print(hobi)
+
+# print('''Halo saya Rendy
+# Saya suka belajar ngoding
+# Hari ini saya''')
+
+# print("Aku""rajin""praktikum")
+# print("Aku" + "rajin")
+
+# teks = "Rahmat Flowchart"
+
+# #tampilin index-3
+# print(teks[3])
+# #tampilin index-3 sampe index
+# print(teks[3:6])
+
+# print(teks[7:])
+
+# print(teks[:6])
+
+# print(teks[1:4:2])
+
+
+# hujan = False
+# panas = True
+
+# biodata = ["Larry", 19, "2026", 82.49, True, ["APD", 26]]
+# print(biodata)
+# mata_kuliah = ["APD","Logika Matematika", "Kalkulus"]
+# print(mata_kuliah[0])
+# print(mata_kuliah[1])
+# print(mata_kuliah[2])
+
+
+# print(mata_kuliah[0:1])
+# print(mata_kuliah[:2])
+# print(mata_kuliah[1:])
+# print(mata_kuliah[::2])
+# print(mata_kuliah[1:])
+# print(mata_kuliah[:-1])
+
+# buku = {
+# 'judul' : 'Atomic Habits',
+# 'penulis' : 'James Clear',
+# 'halaman' : 320
+# }
+# ...
+# # Mengakses Value dalam Dictionary
+# print(buku['judul'])
+# print(buku['penulis'])
+# print(buku['halaman'])
+
+
+# angka = int("42")
+# print(angka)
+# ubah = float("99.99999")
+# print(ubah)
+
+# nama = input("Nama Kamu: ")
+# print(nama)
+
+# gaji = int(input("Gaji Kamu: "))
+# print(gaji)
+
+print(2 < 4)
+print(5 > 5)
+print(2 < 4 and 5 > 10)
+
+zidan = {
+    "pagi" : "kalkulus",
+    "siang" : "Algoritma Pemograman Dasar",
+    "sore" : "bahasa inggris"
+}
+print(zidan["siang"])
